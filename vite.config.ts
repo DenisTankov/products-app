35469c08556516a9a4d4047bb807f9025291bcf3
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2025-04-24 00:57:52 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-   plugins: [react(), svgr(), ghPages()],
+   plugins: [react(), svgr()],
    base: "/products-app/",
    build: {
       outDir: "build",
